c4a79ee8fbc1886c97946e324544eb39af880bfd
zarial <adisak.promwichai@gmail.com>
2019-08-24 15:01:59 +0700

    init 1.1

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 
 export default function(){
     return <div>
-        <h1 className="title">Home</h1>      
+        <h1 className="title">Home Page</h1>      
     </div>
     
 }
